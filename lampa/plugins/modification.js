d8f306461202d6b2ec5abd806dbdf52c35cc14f7
access <jevgeni.kostenko+kde@gmail.com>
2026-01-22 12:37:22 +0200

    update msx start + lampa wrapper

--- lampa/plugins/modification.js
+++ lampa/plugins/modification.js
@@ -29,7 +29,7 @@
         "https://nb557.github.io/plugins/online_mod.js",
 
         "https://skaztv.online/store.js",
-        "https://skaztv.online/js/tricks.js",
+        // "https://skaztv.online/js/tricks.js",
 
         "https://amiv1.github.io/lampa/rating.js",
         "https://amikdn.github.io/buttons.js"
