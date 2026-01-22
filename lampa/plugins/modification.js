42644b8628419446e29b8af728c8a91d97739bd9
access <jevgeni.kostenko+kde@gmail.com>
2026-01-22 11:30:46 +0200

    update msx start + lampa wrapper

--- lampa/plugins/modification.js
+++ lampa/plugins/modification.js
@@ -3,25 +3,25 @@
 
     // список автоплагинов
     const PLUGINS = [
-        "https://skaztv.online/store.js",
-        "http://skaz.tv/vcdn.js",
-        "https://and7ey.github.io/lampa/stats.js",
-        "https://and7ey.github.io/lampa/head_filter.js",
-        "https://andreyurl54.github.io/diesel/tricks.js",
-        "https://bylampa.github.io/redirect.js",
-        "https://bylampa.github.io/lampa/e.js",
-        "https://bylampa.github.io/trailer_off.js",
-        "https://bylampa.github.io/color_vote.js",
-        "https://bylampa.github.io/seas_and_eps.js",
-        "https://bylampa.github.io/old_card_status.js",
-        "https://bylampa.github.io/backmenu.js",
-        "https://bylampa.github.io/cub_off.js",
-
+        // "https://skaztv.online/store.js",
+        // "http://skaz.tv/vcdn.js",
+        // "https://tsynik.github.io/lampa/e.js",
+        // "https://and7ey.github.io/lampa/stats.js",
+        // "https://and7ey.github.io/lampa/head_filter.js",
+        // "https://andreyurl54.github.io/diesel/tricks.js",
+        // "https://bylampa.github.io/redirect.js",
+        // "https://bylampa.github.io/trailer_off.js",
+        // "https://bylampa.github.io/color_vote.js",
+        // "https://bylampa.github.io/seas_and_eps.js",
+        // "https://bylampa.github.io/old_card_status.js",
+        // "https://bylampa.github.io/backmenu.js",
+        // "https://bylampa.github.io/cub_off.js",
+        //
         "https://bwa.to/rc",
         "https://nb557.github.io/plugins/online_mod.js",
         "https://bwa.to/cloud.js",
         "https://skaztv.online/store.js",
-        "https://bylampa.github.io/addon.js",
+        // "https://bylampa.github.io/addon.js",
         "https://amiv1.github.io/lampa/rating.js",
         "https://amikdn.github.io/buttons.js"
     ];
