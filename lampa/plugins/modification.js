065edd2d159057757d24e3bff36a07527d69c122
access <jevgeni.kostenko+kde@gmail.com>
2026-01-22 13:45:55 +0200

    update msx start + lampa wrapper

--- lampa/plugins/modification.js
+++ lampa/plugins/modification.js
@@ -3,15 +3,12 @@
 
     // список автоплагинов (НЕ УДАЛЯЮ закомментированные — оставляю как есть)
     const PLUGINS = [
-        // "http://skaz.tv/onlines.js",
-        // "http://skaz.tv/vcdn.js",
-        // "https://netfix.cc/netfix.js",
         "https://tsynik.github.io/lampa/e.js",
-        //"https://and7ey.github.io/lampa/stats.js",
+        "https://and7ey.github.io/lampa/stats.js",
         "https://and7ey.github.io/lampa/head_filter.js",
-        //"https://andreyurl54.github.io/diesel5/tricks.js",
+        "https://andreyurl54.github.io/diesel5/tricks.js",
 
-        // "https://bylampa.github.io/redirect.js",
+        "https://bylampa.github.io/redirect.js",
         "https://bylampa.github.io/trailer_off.js",
         "https://bylampa.github.io/color_vote.js",
         "https://bylampa.github.io/seas_and_eps.js",
@@ -20,16 +17,16 @@
         "https://bylampa.github.io/cub_off.js",
         "https://bylampa.github.io/addon.js",
 
-        // "https://bdvburik.github.io/title.js",
+        "https://bdvburik.github.io/title.js",
         "https://bywolf88.github.io/lampa-plugins/interface_mod.js",
 
-        // "https://bwa.to/rc",
-        // "https://bwa.to/cloud.js",
+        "https://bwa.to/rc",
+        "https://bwa.to/cloud.js",
 
         "https://nb557.github.io/plugins/online_mod.js",
 
-        // "https://skaztv.online/store.js",
-        // "https://skaztv.online/js/tricks.js",
+        "https://skaztv.online/store.js",
+        //"https://skaztv.online/js/tricks.js",
 
         "https://amiv1.github.io/lampa/rating.js",
         "https://amikdn.github.io/buttons.js"
