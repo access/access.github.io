c8fe1e861d0d1cca8bc3c4a1f0e39bd3a091f66f
access <jevgeni.kostenko+kde@gmail.com>
2026-01-22 13:52:05 +0200

    update msx start + lampa wrapper

--- lampa/plugins/modification.js
+++ lampa/plugins/modification.js
@@ -5,7 +5,7 @@
     const PLUGINS = [
         "https://tsynik.github.io/lampa/e.js",
         "https://and7ey.github.io/lampa/stats.js",
-        "https://and7ey.github.io/lampa/head_filter.js",
+        //"https://and7ey.github.io/lampa/head_filter.js",
         "https://andreyurl54.github.io/diesel5/tricks.js",
 
         "https://bylampa.github.io/redirect.js",
