832a7d1b4a864d85d26f0fac45454022a5f73b73
access <jevgeni.kostenko+kde@gmail.com>
2026-01-22 10:59:45 +0200

    update msx start + lampa wrapper

--- lampa/plugins/modification.js
+++ lampa/plugins/modification.js
@@ -1,9 +1,10 @@
 (function () {
     'use strict';
 
-    // список плагинов, которые надо подключить при старте
+    // список автоплагинов
     const PLUGINS = [
-        // удалённые
+        // пример
+        // './plugins/test.js',
         "https://bwa.to/rc",
         "https://nb557.github.io/plugins/online_mod.js",
         "https://bwa.to/cloud.js",
@@ -11,8 +12,8 @@
         "https://bylampa.github.io/addon.js"
         "https://amiv1.github.io/lampa/rating.js",
         "https://amikdn.github.io/buttons.js",
-        // или из твоего же GitHub Pages (лучше так)
-        // './plugins/my-plugin.js',
+        // реальные
+        // 'https://example.com/plugin.js'
     ];
 
     function load(url) {
@@ -21,27 +22,28 @@
                 const s = document.createElement('script');
                 s.src = url;
                 s.async = true;
-                s.onload = () => resolve({ url, ok: true });
-                s.onerror = () => resolve({ url, ok: false });
+                s.onload = () => resolve(true);
+                s.onerror = () => resolve(false);
                 document.head.appendChild(s);
             } catch (e) {
-                resolve({ url, ok: false, err: e });
+                resolve(false);
             }
         });
     }
 
-    async function start() {
-        // ждать пока Lampa инициализируется, чтобы плагины не падали от отсутствия API
-        const wait = (ms) => new Promise(r => setTimeout(r, ms));
-
-        for (let i = 0; i < 120; i++) { // ~60 сек
-            if (window.Lampa && window.Lampa.Listener) break;
-            await wait(500);
+    async function waitLampa() {
+        for (let i = 0; i < 120; i++) {
+            if (window.Lampa && window.Lampa.Listener) return;
+            await new Promise(r => setTimeout(r, 500));
         }
+    }
+
+    async function start() {
+        await waitLampa();
 
         for (const url of PLUGINS) {
-            const r = await load(url);
-            console.log('[AutoPlugin]', r.ok ? 'OK' : 'FAIL', r.url);
+            const ok = await load(url);
+            console.log('[AutoPlugin]', ok ? 'OK' : 'FAIL', url);
         }
     }
 
