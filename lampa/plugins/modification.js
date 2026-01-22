1d9d8e89673fbaf7331f373939df3f8a56f268f0
access <jevgeni.kostenko+kde@gmail.com>
2026-01-22 15:51:02 +0200

    update msx start + lampa wrapper

--- lampa/plugins/modification.js
+++ lampa/plugins/modification.js
@@ -24,7 +24,7 @@
         "https://bywolf88.github.io/lampa-plugins/interface_mod.js",
 
         // "https://bwa.to/rc",
-        // "https://bwa.to/cloud.js",
+        "https://bwa.to/cloud.js",
 
         "https://nb557.github.io/plugins/online_mod.js",
 
