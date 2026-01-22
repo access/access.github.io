233c04fadf57ad261694cddc1e707e532e63e966
access <jevgeni.kostenko+kde@gmail.com>
2026-01-22 19:51:06 +0200

    update msx start + lampa wrapper

--- lampa/plugins/modification.js
+++ lampa/plugins/modification.js
@@ -47,7 +47,7 @@
     // =========================================================================
     const PLUGINS = [
         "https://bdvburik.github.io/title.js",
-        "https://amikdn.github.io/buttons.js"
+        "https://amikdn.github.io/buttons.js",
         "https://bywolf88.github.io/lampa-plugins/interface_mod.js",
         "scripts/rating.js",
         // "http://skaz.tv/onlines.js",
@@ -82,7 +82,7 @@
         "https://skaztv.online/store.js",
         "https://skaztv.online/js/tricks.js",
 
-        "https://aviamovie.github.io/surs.js",
+        "https://aviamovie.github.io/surs.js"
         // "https://amiv1.github.io/lampa/rating.js",
 
     ];
