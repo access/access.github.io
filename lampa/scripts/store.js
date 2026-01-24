daf87c29718fb83fda33df18c472509a35937955
access <jevgeni.kostenko+kde@gmail.com>
2026-01-24 13:34:57 +0200

    update msx start + lampa wrapper

--- lampa/scripts/store.js
+++ lampa/scripts/store.js
@@ -23,7 +23,7 @@
         if (e.name == 'main') {
             e.body.find('[data-component="pirate_store"]').on('hover:enter', function () {
                 Lampa.Extensions.show({
-                    store: 'extensions.json',
+                    store: 'store.extensions.json',
                     with_installed: true
                 });
             });
